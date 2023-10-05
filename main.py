68bc4924739b6cb456a34a172d69f1b1a1d2bc8c
baileystarr4 <baileystarrc4@gmail.com>
2023-10-05 17:46:37 -0400

    Use user entered time for a work and break countdown

--- main.py
+++ main.py
@@ -20,33 +20,38 @@ timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
 def clicked_reset_button():
     window.after_cancel(timer)
+    canvas.grid_remove()
     timer_label.config(text="Timer")
-    # canvas.itemconfig(timer_text, text="00:00")
+    question_label.config(text="How long would you like to work? \n Enter in minutes")
+    question_label.grid()
+    entry.grid()
+    button.config(text="save", command=save_time_work)
+    
     global reps
     reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
+    global work_min
+    global snooze_min
     reps += 1
 
-    # work_sec = work_min * 60
-    # short_sec = SHORT_BREAK_MIN * 60
-    # long_sec = LONG_BREAK_MIN * 60
+    work_sec = work_min * 60
+    snooze_sec = snooze_min * 60
+
+    entry.grid_remove()
+    canvas.grid(column=1, row=1)
     
-    # if reps % 8 == 0:
-    #     count_down(long_sec)
-    #     timer_label.config(text="Break", fg=DEFAULT_TEXT)
-    # elif reps % 2 == 0:
-    #     count_down(short_sec)
-    #     timer_label.config(text="Break", fg=DEFAULT_TEXT)
-    # else:
-    #     count_down(work_sec)
-    #     timer_label.config(text="Work", fg=DEFAULT_TEXT)
+    if reps % 2 == 0:
+        count_down(snooze_sec)
+        timer_label.config(text="Break", fg=DEFAULT_TEXT)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=DEFAULT_TEXT)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_min < 10:
@@ -54,19 +59,31 @@ def count_down(count):
     if count_sec < 10:
         count_sec=f"0{count_sec}"
 
-    # canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         global timer
         timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
 
-# ----------------------------------------------------------- # 
+# ----------------------------------------------------------- #
 def save_time_work():
-    input = work_time_entry.get()
+    input = int(entry.get())
     global work_min
     work_min = input
-    timer_label.config(text=work_min)
+
+    button.config(command=save_time_snooze)
+    question_label.config(text="How long would you like to snooze?\n Enter in minutes")
+
+def save_time_snooze():
+    input = int(entry.get())
+    global snooze_min
+    snooze_min = input
+
+    question_label.grid_remove()
+    button.config(text="result", command=clicked_reset_button)
+    start_timer()
+
     
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -75,21 +92,23 @@ window.config(padx=150, pady=100, bg=DEFAULT_BG)
 
 timer_label = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=DEFAULT_TEXT, bg=DEFAULT_BG)
 timer_label.grid(column=1, row=0)
-
-work_time_entry = Entry(window, width=10)
-work_time_entry.grid(column=1, row=1)
-
-
+question_label = Label(text="How long would you like to work? \n Enter in minutes", font=(FONT_NAME, 18), fg=DEFAULT_TEXT, bg=DEFAULT_BG)
+question_label.grid(column=1, row=1)
+entry = Entry(window, width=10)
+entry.grid(column=1, row=2)
 button = Button(text="save", command=save_time_work)
-button.grid(column=1, row=2)
+button.grid(column=1, row=3)
+
+
+
+
 # start_button = Button(text="Start", command=start_timer)
 # start_button.grid(column=0, row=2)
 
 # reset_button = Button(text="Reset", command=clicked_reset_button)
 # reset_button.grid(column=2, row=2)
 
-# canvas = Canvas(width=200, height=224, bg=DEFAULT_BG, highlightthickness=0)
-# timer_text = canvas.create_text(100, 130, text="00:00", fill = "white", font=(FONT_NAME, 35, "bold"))
-# canvas.grid(column=1, row=1)
+canvas = Canvas(width=200, height=224, bg=DEFAULT_BG, highlightthickness=0)
+timer_text = canvas.create_text(100, 130, text="00:00", fill = "white", font=(FONT_NAME, 35, "bold"))
 
 window.mainloop()
