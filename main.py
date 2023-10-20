d375c2f82685e499f3f8c51dc71b4832025263e4
baileystarr4 <baileystarrc4@gmail.com>
2023-10-20 10:31:57 -0400

    Changing notification sound and fixing path to icon image

--- main.py
+++ main.py
@@ -184,8 +184,10 @@ def save_time_snooze():
 
 # ---------------------------- PLAY ALARM ------------------------------- # 
 def play():
-    pygame.mixer.music.load("alarm.wav")
-    pygame.mixer.music.play(loops=1)
+    # Notification sound by Joao_Janz on freesound.org
+    # https://freesound.org/people/Joao_Janz/sounds/504821/
+    pygame.mixer.music.load("notification_sound.wav")
+    pygame.mixer.music.play(loops=3)
 
 # ---------------------------- UI ------------------------------- # 
 #Initialize and configure window
@@ -218,5 +220,5 @@ pygame.mixer.init()
 #Initializing windows notification 
 # Icon by Leremy https://www.freepik.com/icon/healthy_10049659#fromView=search&term=stretching&page=1&position=4&track=ais
 toast = Notification(app_id="Stretch Timer", title="Get up and stretch!",
-                     duration = "short", icon="C:/Users/Bailey/Documents/GitHub/Timer-App/notification_icon.png")
+                     duration = "short", icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\notificaiton_icon.png")
 window.mainloop()
