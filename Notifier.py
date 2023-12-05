2a32aed2c14776971c3f21d5ba06b71f7c9d64a8
baileystarr4 <baileystarrc4@gmail.com>
2023-12-05 14:43:35 -0500

    Add stop_music method and fix bugs in Notifier

--- Notifier.py
+++ Notifier.py
@@ -1,17 +1,21 @@
 import pygame
 from winotify import Notification
-
 class Notifier:
     def __init__(self):
-        self.alarm = pygame.mixer.init()
+        pygame.mixer.init()
 
         #Initializing windows notification 
         # Icon by Leremy https://www.freepik.com/icon/healthy_10049659#fromView=search&term=stretching&page=1&position=4&track=ais
-        self.toast = Notification(app_id="Stretch Timer", title="Get up and stretch!",
-                     duration = "short", icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\notificaiton_icon.png")
+        self.work_toast = Notification(app_id="Stretch Timer", duration = "short", title="Time to work!",
+                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\notificaiton_icon.png")
+        self.break_toast =Notification(app_id="Stretch Timer", duration = "short", title="Time to break!",
+                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\notificaiton_icon.png")
 
     def play_alarm(self):
         # Notification sound by Joao_Janz on freesound.org
         # https://freesound.org/people/Joao_Janz/sounds/504821/
-        self.alarm.music.load("notification_sound.wav")
-        self.alarm.music.play(loops=3)
+        pygame.mixer.music.load("notification_sound.wav")
+        pygame.mixer.music.play(loops=3)
+    
+    def stop_music(self):
+        pygame.mixer.music.stop()
