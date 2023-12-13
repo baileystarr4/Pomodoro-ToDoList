aae6d0c9e4d4f9b09c3500e840cccd0c65643759
baileystarr4 <baileystarrc4@gmail.com>
2023-12-13 02:53:37 -0500

    Add new icons to to do list frame

--- ToDoList.py
+++ ToDoList.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 class ToDoList:
     def __init__(self, window):
         self.w = window
-        self.img1 = ImageTk.PhotoImage(Image.open("open.png"))
+        self.img1 = ImageTk.PhotoImage(Image.open("icons/list_icon.png"))
         self.open_to_do = Button(self.w, image=self.img1, command=self.toggle_win, border=0, bg="#272829", activebackground="#272829")
         self.open_to_do.place(x=10,y=10)
         self.tasks = defaultdict(Button)
@@ -19,9 +19,22 @@ class ToDoList:
         self.open_to_do.place_forget()
         self.f1 = Frame(self.w,width=300,height=500,bg='#5C8374')
         self.f1.place(x=0,y=0)
-        self.img2 = ImageTk.PhotoImage(Image.open("close.png"))
+
+        self.img2 = ImageTk.PhotoImage(Image.open("icons/close_icon.png"))
         self.close_to_do = Button(self.f1, image=self.img2, border=0, command=self.dele, bg='#5C8374', activebackground='#5C8374')
         self.close_to_do.place(x=250,y=10)
+
+        self.img3 = ImageTk.PhotoImage(Image.open("icons/add_icon.png"))
+        self.add_task = Button(self.f1, image=self.img3, border=0, command=self.dele, bg='#5C8374', activebackground='#5C8374')
+        self.add_task.place(x=50,y=430)
+
+        self.img4 = ImageTk.PhotoImage(Image.open("icons/refresh_icon.png"))
+        self.refresh_to_do = Button(self.f1, image=self.img4, border=0, command=self.dele, bg='#5C8374', activebackground='#5C8374')
+        self.refresh_to_do.place(x=120,y=430)
+
+        self.img5 = ImageTk.PhotoImage(Image.open("icons/trash_icon.png"))
+        self.trash_to_do = Button(self.f1, image=self.img5, border=0, command=self.dele, bg='#5C8374', activebackground='#5C8374')
+        self.trash_to_do.place(x=190,y=430)
         
         self.create_tasks()
 
@@ -66,6 +79,3 @@ class ToDoList:
         self.f1.place_forget()
         self.open_to_do.place(x=5,y=10)
         self.update_csv()
-
-
-        
