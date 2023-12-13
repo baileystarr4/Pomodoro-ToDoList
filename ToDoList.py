38bb5730c8b5a37a72ef8a5cf74c6e9db368cab7
baileystarr4 <baileystarrc4@gmail.com>
2023-12-13 03:03:46 -0500

    Fix write to csv bug
    corrected bug where to do list was not adding tasks outside of the frame scope back to the csv.

--- ToDoList.py
+++ ToDoList.py
@@ -9,7 +9,7 @@ class ToDoList:
         self.img1 = ImageTk.PhotoImage(Image.open("icons/list_icon.png"))
         self.open_to_do = Button(self.w, image=self.img1, command=self.toggle_win, border=0, bg="#272829", activebackground="#272829")
         self.open_to_do.place(x=10,y=10)
-        self.tasks = defaultdict(Button)
+        self.tasks = defaultdict(None)
         
         # self.active_task_font = font.Font(family="Courier", overstrike = 0)
         self.active_task_font = ("Courier", 12)
@@ -45,21 +45,22 @@ class ToDoList:
         df = pandas.read_csv('ToDoList.csv')
 
         for index, row in df.iterrows():
-            if index == 10:
-                break
-            new_button = Button(self.f1,text=row["Task"],
-                           font= self.active_task_font,
-                           justify= LEFT,
-                           wraplength=250,
-                           fg='#040D12',
-                           border=0,
-                           bg='#5C8374',
-                           activeforeground='#040D12',
-                           activebackground='#5C8374')
-            new_button.config(command=lambda b = new_button: self.cross_off_task(b))
-            new_button.place(x=x, y=y, anchor='w')
-            self.tasks[row["Task"]] = new_button
-            y += 40
+            if index < 10:
+                new_button = Button(self.f1,text=row["Task"],
+                            font= self.active_task_font,
+                            justify= LEFT,
+                            wraplength=250,
+                            fg='#040D12',
+                            border=0,
+                            bg='#5C8374',
+                            activeforeground='#040D12',
+                            activebackground='#5C8374')
+                new_button.config(command=lambda b = new_button: self.cross_off_task(b))
+                new_button.place(x=x, y=y, anchor='w')
+                self.tasks[row["Task"]] = new_button
+                y += 40
+            else:
+                self.tasks[row['Task']] = None
 
     def cross_off_task(self, button):
         button.config(font=self.finished_task_font, command= lambda b = button: self.uncross_off_task(b))
@@ -70,7 +71,7 @@ class ToDoList:
     def update_csv(self):
         remaining_tasks = {'Task': []}
         for task, button in self.tasks.items():
-            if "overstrike" not in button.cget("font"):
+            if not button or "overstrike" not in button.cget("font"):
                 remaining_tasks['Task'].append(task)
         df = pandas.DataFrame(remaining_tasks)
         df.to_csv('ToDoList.csv', index=False)
