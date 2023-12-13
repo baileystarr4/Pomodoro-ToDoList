75e807feef4bd09e4449ede5bddd8c02fc0d8414
baileystarr4 <baileystarrc4@gmail.com>
2023-12-13 02:52:33 -0500

    Read and write to to do list csv
    Read tasks from csv and add them to the to do list frame. Then, when the window is closed, write only the remaining tasks to the csv file.

--- ToDoList.py
+++ ToDoList.py
@@ -1,12 +1,15 @@
 from tkinter import *
 from PIL import ImageTk,Image
+import pandas
+from collections import defaultdict
 
 class ToDoList:
     def __init__(self, window):
         self.w = window
         self.img1 = ImageTk.PhotoImage(Image.open("open.png"))
-        self.open_to_do = Button(self.w, image=self.img1, command=self.toggle_win, border=0, bg='#040D12', activebackground='#183D3D')
-        self.open_to_do.place(x=5,y=10)
+        self.open_to_do = Button(self.w, image=self.img1, command=self.toggle_win, border=0, bg="#272829", activebackground="#272829")
+        self.open_to_do.place(x=10,y=10)
+        self.tasks = defaultdict(Button)
         
         # self.active_task_font = font.Font(family="Courier", overstrike = 0)
         self.active_task_font = ("Courier", 12)
@@ -18,19 +21,20 @@ class ToDoList:
         self.f1.place(x=0,y=0)
         self.img2 = ImageTk.PhotoImage(Image.open("close.png"))
         self.close_to_do = Button(self.f1, image=self.img2, border=0, command=self.dele, bg='#5C8374', activebackground='#5C8374')
-        self.close_to_do.place(x=5,y=10)
+        self.close_to_do.place(x=250,y=10)
         
         self.create_tasks()
 
 
     def create_tasks(self):
         x = 10
-        y = 80
-        tasks = ["sleep", "eat", "study"]
-        
+        y = 40
+        df = pandas.read_csv('ToDoList.csv')
 
-        for task in tasks:
-            new_button = Button(self.f1,text=task,
+        for index, row in df.iterrows():
+            if index == 10:
+                break
+            new_button = Button(self.f1,text=row["Task"],
                            font= self.active_task_font,
                            justify= LEFT,
                            wraplength=250,
@@ -39,9 +43,9 @@ class ToDoList:
                            bg='#5C8374',
                            activeforeground='#040D12',
                            activebackground='#5C8374')
-
             new_button.config(command=lambda b = new_button: self.cross_off_task(b))
             new_button.place(x=x, y=y, anchor='w')
+            self.tasks[row["Task"]] = new_button
             y += 40
 
     def cross_off_task(self, button):
@@ -49,10 +53,19 @@ class ToDoList:
 
     def uncross_off_task(self, button):
         button.config(font=self.active_task_font, command= lambda b = button: self.cross_off_task(b))
+    
+    def update_csv(self):
+        remaining_tasks = {'Task': []}
+        for task, button in self.tasks.items():
+            if "overstrike" not in button.cget("font"):
+                remaining_tasks['Task'].append(task)
+        df = pandas.DataFrame(remaining_tasks)
+        df.to_csv('ToDoList.csv', index=False)
 
     def dele(self):
         self.f1.place_forget()
         self.open_to_do.place(x=5,y=10)
+        self.update_csv()
 
 
         
