333285d953d06b9c7fa63394317c931e861d6026
baileystarr4 <baileystarrc4@gmail.com>
2023-12-22 16:01:10 -0500

    Add Zelda sounds and new alert icon

--- Notifier.py
+++ Notifier.py
@@ -4,20 +4,26 @@ class Notifier:
     def __init__(self):
         pygame.mixer.init()
 
-        #Initializing windows notification 
-        # Icon by Leremy https://www.freepik.com/icon/healthy_10049659#fromView=search&term=stretching&page=1&position=4&track=ais
+        # Creating a different notification for work, long break, short break, and end
         self.work_toast = Notification(app_id="Pomodoro", duration = "short", title="Time to work!",
-                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\notificaiton_icon.png")
+                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
         self.long_break_toast =Notification(app_id="Pomodoro", duration = "short", title="Time for a long break!",
-                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\notificaiton_icon.png")
+                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
         self.short_break_toast =Notification(app_id="Pomodoro", duration = "short", title="Time for a short break!",
-                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\notificaiton_icon.png")
+                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
+        self.end_toast =Notification(app_id="Pomodoro", duration = "short", title="You have completed all your pomodoros! Congrats!",
+                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
+        
+    # Notification sounds from https://noproblo.dayjo.org/ZeldaSounds/ 
+    def play_sound(self, sound):
+        if sound == "break":
+            pygame.mixer.music.load("notifier_sounds/BOTW_Fanfare_SmallItem.wav")
+        elif sound == "work":
+            pygame.mixer.music.load("notifier_sounds/BOTW_Secret.wav")
+        elif sound == "end":
+            pygame.mixer.music.load("notifier_sounds/BOTW_Fanfare_SpiritOrb.wav")
 
-    def play_alarm(self):
-        # Notification sound by Joao_Janz on freesound.org
-        # https://freesound.org/people/Joao_Janz/sounds/504821/
-        pygame.mixer.music.load("notification_sound.wav")
-        pygame.mixer.music.play(loops=3)
-    
+        pygame.mixer.music.play()
+
     def stop_music(self):
         pygame.mixer.music.stop()
