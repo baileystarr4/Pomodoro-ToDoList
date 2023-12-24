d9c9e2c2b5053c3baf57d21504e087dae299b509
baileystarr4 <baileystarrc4@gmail.com>
2023-12-23 20:24:55 -0500

    Clean up notifier

--- Notifier.py
+++ Notifier.py
@@ -1,10 +1,11 @@
 import pygame
 from winotify import Notification
+
 class Notifier:
     def __init__(self):
         pygame.mixer.init()
 
-        # Creating a different notification for work, long break, short break, and end
+        # Intialize a different notification for work, long break, short break, and end.
         self.work_toast = Notification(app_id="Pomodoro", duration = "short", title="Time to work!",
                                   icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
         self.long_break_toast =Notification(app_id="Pomodoro", duration = "short", title="Time for a long break!",
@@ -14,16 +15,18 @@ class Notifier:
         self.end_toast =Notification(app_id="Pomodoro", duration = "short", title="You have completed all your pomodoros! Congrats!",
                                   icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
         
-    # Notification sounds from https://noproblo.dayjo.org/ZeldaSounds/ 
-    def play_sound(self, sound):
-        if sound == "break":
+    def play_sound(self, session):
+    # This method plays the correct sound given what session is about to start.
+    # Notification sounds from https://noproblo.dayjo.org/ZeldaSounds/
+        
+        if session == "break":
             pygame.mixer.music.load("notifier_sounds/BOTW_Fanfare_SmallItem.wav")
-        elif sound == "work":
+        elif session == "work":
             pygame.mixer.music.load("notifier_sounds/BOTW_Secret.wav")
-        elif sound == "end":
+        elif session == "end":
             pygame.mixer.music.load("notifier_sounds/BOTW_Fanfare_SpiritOrb.wav")
 
         pygame.mixer.music.play()
 
-    def stop_music(self):
+    def stop_sound(self):
         pygame.mixer.music.stop()
