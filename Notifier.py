c425388685e33de6a22cbea5cb9b26a08e165e08
baileystarr4 <baileystarrc4@gmail.com>
2023-12-26 15:11:35 -0500

    Delete notifier icons
    notification icons would not work in exe files because of the absolute path.

--- Notifier.py
+++ Notifier.py
@@ -6,14 +6,10 @@ class Notifier:
         pygame.mixer.init()
 
         # Intialize a different notification for work, long break, short break, and end.
-        self.work_toast = Notification(app_id="Pomodoro", duration = "short", title="Time to work!",
-                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
-        self.long_break_toast =Notification(app_id="Pomodoro", duration = "short", title="Time for a long break!",
-                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
-        self.short_break_toast =Notification(app_id="Pomodoro", duration = "short", title="Time for a short break!",
-                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
-        self.end_toast =Notification(app_id="Pomodoro", duration = "short", title="You have completed all your pomodoros! Congrats!",
-                                  icon=r"C:\Users\Bailey\Documents\GitHub\Timer-App\icons\notifier_icon.png")
+        self.work_toast = Notification(app_id="Pomodoro", duration = "short", title="Time to work!")
+        self.long_break_toast =Notification(app_id="Pomodoro", duration = "short", title="Time for a long break!")
+        self.short_break_toast =Notification(app_id="Pomodoro", duration = "short", title="Time for a short break!")
+        self.end_toast =Notification(app_id="Pomodoro", duration = "short", title="You have completed all your pomodoros! Congrats!")
         
     def play_sound(self, session):
     # This method plays the correct sound given what session is about to start.
